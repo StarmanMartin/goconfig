ba1ddbc773cbce7f3b670083accb27dbacf36332
Maritn Starman <m.starman@live.com>
2016-07-25 21:43:23 +0200

    added must getter

--- interface.go
+++ interface.go
@@ -2,25 +2,30 @@ package goconfig
 
 import (
 	"errors"
-    "fmt"
+	"fmt"
+	"log"
 )
 
 var c *config
 
 func InitConficOnce(mainConfig string, files ...string) error {
+	return InitConfigOnce(mainConfig, files...)
+}
+
+func InitConfigOnce(mainConfig string, files ...string) error {
 	val, err := readJSON(mainConfig)
 	if err != nil {
 		return errors.New("Main config: " + err.Error())
 	}
-    
-    for _, file := range files {
-        
-        if value, errI := readJSON(file); errI == nil {
-            val = margeJSON(val, value)
-        } else {
-            fmt.Println(errI)
-        }
-    }
+
+	for _, file := range files {
+
+		if value, errI := readJSON(file); errI == nil {
+			val = margeJSON(val, value)
+		} else {
+			fmt.Println(errI)
+		}
+	}
 
 	c = newConfig(val)
 
@@ -97,4 +102,108 @@ func GetArrayBool(keyWords ...string) ([]bool, bool) {
 	}
 
 	return c.getArrayBool(keyWords...)
+}
+
+func MustGetString(keyWords ...string) (string) {
+	if c == nil {
+		return ""
+	}
+
+	val, ok := c.getString(keyWords...)
+	if !ok {
+		log.Panic("Value not found!")
+	}
+
+	return val
+}
+
+func MustGetFloat(keyWords ...string) (float64) {
+	if c == nil {
+		return 0
+	}
+
+	val, ok := c.getFloat(keyWords...)
+	if !ok {
+		log.Panic("Value not found!")
+	}
+
+	return val
+}
+
+func MustGetInt(keyWords ...string) (int) {
+	if c == nil {
+		return 0
+	}
+
+	val, ok := c.getInt(keyWords...)
+	if !ok {
+		log.Panic("Value not found!")
+	}
+
+	return val
+}
+
+func MustGetBool(keyWords ...string) (bool) {
+	if c == nil {
+		return false
+	}
+
+	val, ok := c.getBool(keyWords...)
+	if !ok {
+		log.Panic("Value not found!")
+	}
+
+	return val
+}
+
+func MustGetArrayString(keyWords ...string) ([]string) {
+	if c == nil {
+		return nil
+	}
+
+	val, ok := c.getArrayString(keyWords...)
+	if !ok {
+		log.Panic("Value not found!")
+	}
+
+	return val
+}
+
+func MustGetArrayFloat(keyWords ...string) ([]float64) {
+	if c == nil {
+		return nil
+	}
+
+	val, ok := c.getArrayFloat(keyWords...)
+	if !ok {
+		log.Panic("Value not found!")
+	}
+
+	return val
+}
+
+func MustGetArrayInt(keyWords ...string) ([]int) {
+	if c == nil {
+		return nil
+	}
+
+	val, ok := c.getArrayInt(keyWords...)
+	if !ok {
+		log.Panic("Value not found!")
+	}
+
+	return val
+}
+
+func MustGetArrayBool(keyWords ...string) ([]bool) {
+	if c == nil {
+		return nil
+	}
+
+	val, ok := c.getArrayBool(keyWords...)
+	if !ok {
+		log.Panic("Value not found!")
+	}
+
+	return val
 }
